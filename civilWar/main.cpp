fe44c03ae88e4153bab0545d771646268abd028f
曾子舰 <zzj@zzj.local>
2015-11-21 17:38:12 +0800

    Initial Commit

---
+++ civilWar/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  civilWar
+//
+//  Created by ZZJ on 15/11/21.
+//  Copyright © 2015年 ZZJ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
